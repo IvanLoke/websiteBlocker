b403da40dfa069bff529525067a8cef8f0c83d1c
IvanLoke <ivanloke0305@gmail.com>
2024-12-12 09:28:00 +0700

    Initial commit, working website blocker

---
+++ app.go
@@ -0,0 +1,543 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+var (
+	goroutineContexts = make(map[string]context.CancelFunc) // Map to hold cancel functions
+	mu                sync.Mutex                            // Mutex to protect access to the map
+	wg                sync.WaitGroup
+)
+
+var prefixes = [3]string{"www.", "https://", "http://"}
+var suffixes = [2]string{".com", ".org"}
+
+// Site represents a single site to block
+type Site struct {
+	Name     string `yaml:"name"`
+	URL      string `yaml:"url"`
+	Duration string `yaml:"duration"`
+}
+
+// Header of yaml file with all sites
+type HeaderSite struct {
+	Sites []Site `yaml:"sites"`
+}
+
+// Function to read yaml file, returns a HeaderSite struct
+func readYamlFile(filename string) (HeaderSite, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return HeaderSite{}, err
+	}
+	defer file.Close()
+
+	var headerSites HeaderSite
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&headerSites)
+	if err != nil {
+		return HeaderSite{}, err
+	}
+	return headerSites, nil
+}
+
+// Function to format sites from yaml file
+func formatString(data string) string {
+	return strings.TrimSpace(strings.ToLower(data))
+}
+
+// Function to extract name from url
+func getNameFromURL(url string) string {
+	for _, prefix := range prefixes {
+		url = strings.TrimPrefix(url, prefix)
+	}
+	for _, suffix := range suffixes {
+		url = strings.TrimSuffix(url, suffix)
+	}
+	return formatString(url)
+}
+
+// Function to add new goroutine when editing or adding to yaml file
+func addNewGoroutine(url string, expiryTime time.Time) {
+	ctx, cancel := context.WithCancel(context.Background()) // Create a new context for each site
+	mu.Lock()
+	goroutineContexts[url] = cancel // Use the site URL as the key
+	mu.Unlock()
+	go func(expiry time.Time, url string, ctx context.Context) {
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done(): // Check if context is cancelled through the cancel() function in cleanup
+				fmt.Printf("Goroutine for %s cancelled.\n", url)
+				return
+			case <-ticker.C: // Counter to automatically remove site after expiry time
+				if time.Now().After(expiry) {
+					cleanup(false, url) // Replace with actual cleanup logic
+					fmt.Printf("Cleaned up 1%s\n", url)
+					showMenu()
+					return
+				}
+			}
+		}
+	}(expiryTime, url, ctx)
+}
+
+// Function to write to yaml file
+func writeToYamlFile(filename string, name string, url string, expiryTimeString string, expiryTime time.Time) error {
+	// Read yaml file
+	headerSites, err := readYamlFile(filename)
+	formatted_url := formatString(url)
+	if err != nil {
+		return err
+	}
+
+	// Check if site already exists
+	for _, site := range headerSites.Sites {
+		if site.URL == formatted_url {
+			return fmt.Errorf("Site already blocked")
+		}
+	}
+
+	// Add new site to yaml file
+	newSite := Site{
+		Name:     formatString(name),
+		URL:      formatted_url,
+		Duration: expiryTimeString,
+	}
+	headerSites.Sites = append(headerSites.Sites, newSite)
+
+	// Write to original file
+	file, err := os.OpenFile(filename, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.Seek(0, 0)
+	encoder := yaml.NewEncoder(file)
+	if err := encoder.Encode(&headerSites); err != nil {
+		return err
+	}
+
+	addNewGoroutine(url, expiryTime)
+	return nil
+}
+
+// Function to delete site from yaml file
+func deleteSiteFromYamlFile(filename string, name, url string) error {
+
+	// Read yaml file
+	headerSites, err := readYamlFile(filename)
+	if err != nil {
+		return err
+	}
+
+	// Remove site from yaml file
+	var updatedSites []Site
+	name = strings.TrimSpace(strings.ToLower(name))
+	for _, site := range headerSites.Sites {
+		if name != "" && site.Name != name {
+			updatedSites = append(updatedSites, site)
+		} else if site.URL != url {
+			updatedSites = append(updatedSites, site)
+		}
+	}
+	headerSites.Sites = updatedSites
+
+	//Write and truncate original file
+	file, err := os.OpenFile(filename, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := file.Seek(0, 0); err != nil {
+		return err
+	}
+
+	encoder := yaml.NewEncoder(file)
+	if err := encoder.Encode(&headerSites); err != nil {
+		return err
+	}
+
+	pos, err := file.Seek(0, 1)
+	if err != nil {
+		return err
+	}
+	if err := file.Truncate(pos); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to update the expiry time for blocked sites
+func updateExpiryTime(filename string, url string, newExpiryTime time.Time, alreadyExists bool) error {
+	newExpiryTimeStr := newExpiryTime.Format("2006-01-02 15:04:05 -0700")
+	sites, err := readYamlFile(filename)
+	if err != nil {
+		return err
+	}
+	if len(sites.Sites) == 0 {
+		fmt.Println("No sites in config file")
+		return nil
+	}
+
+	// Iterating through sites to find if requested site is blocked
+	siteExists := false
+	for i := range sites.Sites {
+		if sites.Sites[i].URL == url {
+			sites.Sites[i].Duration = newExpiryTimeStr
+			siteExists = true
+			break
+		}
+	}
+
+	if !siteExists {
+		fmt.Println("Site not found in config file")
+		return nil
+	}
+
+	// Writing to original file
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := yaml.NewEncoder(file)
+	if err := encoder.Encode(&sites); err != nil {
+		return err
+	}
+
+	if alreadyExists { // bool to check if the site already exists in config, if it does, we need to update the goroutine. If it does not ie. startup, skip
+		fmt.Printf("Updated expiry time for site: %s to %v", url, newExpiryTimeStr)
+		cleanup(false, url)
+		blockSites(false, filename, url)
+		addNewGoroutine(url, newExpiryTime)
+	}
+	return nil
+}
+
+// Fcunction to display the status of the blocked sites
+func displayStatus(fileName string) {
+	status, err := readYamlFile(fileName)
+	if err != nil {
+		return
+	}
+	empty := true
+	for _, site := range status.Sites {
+		parsedTime, err := time.Parse(DateTimeLayout, site.Duration)
+		if err != nil {
+			fmt.Println("Error parsing time:", err)
+			return
+		}
+
+		if parsedTime.Before(time.Now()) {
+			continue
+		}
+		empty = false
+		timeDifference := time.Until(parsedTime)      // Get the duration between parsedTime and current time
+		hours := int(timeDifference.Hours())          // Convert to hours
+		minutes := int(timeDifference.Minutes()) % 60 // Convert to minutes
+		seconds := int(timeDifference.Seconds()) % 60 // Convert to seconds and get the remainder
+
+		fmt.Printf("- %-20s Time remaining: %d hours %d minutes and %d seconds\n", site.URL, hours, minutes, seconds)
+		fmt.Printf("- %-20s Expiry Time: %s\n", site.URL, site.Duration)
+	}
+	if empty {
+		fmt.Println("No sites are currently blocked")
+	}
+}
+
+func showMenu() {
+	fmt.Println("\n=== SelfControl Menu ===")
+	fmt.Println("1. Start new block")
+	fmt.Println("2. Show current status")
+	fmt.Println("3. Edit expiry time for blocked sites")
+	fmt.Println("4. Unblock all sites")
+	fmt.Println("5. Exit")
+	fmt.Println("6: Unblock all sites")
+	fmt.Println("7: Unblock specific sites")
+	fmt.Println("8: Add new site to block")
+	fmt.Println("9: Delete site from yaml configuration")
+	fmt.Print("\nEnter your choice (1-5): ")
+}
+
+func readUserInput(reader *bufio.Reader) string {
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
+}
+
+func getDuration(reader *bufio.Reader) time.Duration {
+	for {
+		fmt.Print("Enter duration (e.g. 10s, 30m, 1h, 2h30m): ")
+		input := readUserInput(reader)
+		duration, err := time.ParseDuration(input)
+		if err != nil {
+			fmt.Println("Invalid duration format. Please try again.")
+			continue
+		}
+		return duration
+	}
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		wg.Wait()
+		showMenu()
+		choice := readUserInput(reader)
+
+		switch choice {
+		case "1":
+			fmt.Println("Chosen to block sites")
+
+			sitesFileLocation := "blocked-sites.yaml"
+			duration := getDuration(reader)
+
+			// Calculate expiry time
+			expiryTime := time.Now().Add(duration)
+			fmt.Println("Expiry Time: ", expiryTime)
+
+			// Block sites
+			if err := blockSites(true, sitesFileLocation, ""); err != nil {
+				fmt.Printf("Error blocking sites: %v\n", err)
+				continue
+			}
+			// Manually activating gourutine for init
+			headerSites, err := readYamlFile(sitesFileLocation)
+			if err != nil {
+				fmt.Printf("Error reading YAML file: %v\n", err)
+				continue
+			}
+			for _, site := range headerSites.Sites {
+				updateExpiryTime(sitesFileLocation, site.URL, expiryTime, false)
+				addNewGoroutine(site.URL, expiryTime)
+			}
+
+		case "2":
+			fmt.Println("Chosen to show current status")
+			displayStatus("blocked-sites.yaml")
+		case "3":
+			fmt.Println("Enter which site to change expiry time")
+			site := readUserInput(reader)
+			fmt.Println("Enter new expiry time")
+			newExpiryTime := time.Now().Add(getDuration(reader))
+			if err := updateExpiryTime("blocked-sites.yaml", site, newExpiryTime, true); err != nil {
+				fmt.Printf("Error updating expiry time: %v\n", err)
+			}
+		case "5":
+			fmt.Println("Goodbye!")
+			cleanup(true, "")
+			return
+		case "6": // Unblock all sites
+			fmt.Println("Unblocked all sites")
+			cleanup(true, "")
+		case "7": // Unblock specific site
+			fmt.Print("Enter site to unblock: ")
+			site := readUserInput(reader)
+			if err := cleanup(false, site); err != nil {
+				fmt.Printf("Error unblocking site: %v\n", err)
+				continue
+			}
+			deleteSiteFromYamlFile("blocked-sites.yaml", "", site)
+			fmt.Println("Unblocked site: ", site)
+		case "8": // Add new site to block
+			fmt.Print("Enter site URL: ")
+			site := readUserInput(reader)
+			fmt.Print("Enter blocking duration: ")
+			duration := readUserInput(reader)
+			parsedDuration, err := time.ParseDuration(duration)
+			if err != nil {
+				fmt.Printf("Invalid duration format: %v\n", err)
+				continue
+			}
+			expiryTime := time.Now().Add(parsedDuration)
+			name := getNameFromURL(site)
+			formattedExpiryTime := expiryTime.Format("2006-01-02 15:04:05 -0700")
+			fmt.Println("Expiry Time: ", formattedExpiryTime)
+			writeToYamlFile("blocked-sites.yaml", name, site, formattedExpiryTime, expiryTime) //ADD DELETESITEFROMYAML TO THIS FUNCTION
+			blockSites(false, "blocked-sites.yaml", site)
+		case "9": // Delete site from yaml configuration
+			fmt.Print("Enter site to delete from Config: ")
+			site := readUserInput(reader)
+			if err := deleteSiteFromYamlFile("blocked-sites.yaml", "", site); err != nil {
+				fmt.Printf("Error deleting site: %v\n", err)
+			}
+		case "a":
+			fmt.Println(getNameFromURL("www.youtube.com"))
+		case "q":
+			originalTimeStr := "2006-01-02 15:04:05.999999999 -0700 -07 m=+14.614541400"
+
+			// Define the original layout
+			originalLayout := "2006-01-02 15:04:05.999999999 -0700 -07 m=+14.614541400"
+
+			// Parse the original time string
+			parsedTime, err := time.Parse(originalLayout, originalTimeStr)
+			if err != nil {
+				fmt.Println("Error parsing time:", err)
+				return
+			}
+
+			// Define the new layout, adjusting for the abbreviation and dropping nanosecond precision
+			newLayout := "2006-01-02 15:04:05 -0700"
+
+			// Format the parsed time to the new layout
+			formattedTime := parsedTime.Format(newLayout)
+
+			fmt.Println("Transformed Time:", formattedTime)
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
+}
+
+// Function to block sites using the specified YAML file and update the /etc/hosts file
+func blockSites(all bool, yamlFile string, specificSite string) error {
+	var sites []string
+
+	if all {
+		// Read sites from the specified YAML file
+		headerSites, err := readYamlFile(yamlFile)
+		if err != nil {
+			return fmt.Errorf("error reading YAML file: %w", err)
+		}
+
+		// Prepare hosts file entries
+		for _, site := range headerSites.Sites {
+			sites = append(sites, site.URL)
+		}
+	} else {
+		sites = append(sites, specificSite)
+	}
+	// Update the hosts file with the new entries
+	if err := updateHostsFile(sites); err != nil {
+		return fmt.Errorf("error updating hosts file: %w", err)
+	}
+
+	return nil
+}
+
+// Function to update the hosts file
+func updateHostsFile(sites []string) error {
+	// Read the current contents of the hosts file
+	content, err := os.ReadFile("/etc/hosts")
+	if err != nil {
+		return err
+	}
+
+	// Open the hosts file for appending
+	file, err := os.OpenFile("/etc/hosts", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close() // Ensure the file is closed once at the end
+
+	// Check if the marker already exists
+	if !strings.Contains(string(content), "# Added by selfcontrol") {
+		// Add the marker
+		if _, err := file.WriteString("# Added by selfcontrol\n"); err != nil {
+			return err
+		}
+	}
+
+	// Add new entries to the hosts file
+	for _, site := range sites {
+		if !strings.Contains(string(content), site) {
+			if _, err := file.WriteString(fmt.Sprintf("127.0.0.1 %s\n", site)); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Removes all entries inside etc/hosts that were added by selfcontrol
+func cleanup(all bool, url string) error {
+	// Read etc/hosts file
+	content, err := os.ReadFile(hostsFile)
+	if err != nil {
+		return fmt.Errorf("error reading hosts file: %v", err)
+	}
+
+	if !all && url == "" {
+		return fmt.Errorf("empty URL")
+	}
+
+	// Read sites from the specified YAML file
+	var sites []string
+	if all {
+		headerSites, err := readYamlFile("blocked-sites.yaml")
+		if err != nil {
+			return err
+		}
+
+		// Prepare hosts file entries
+		for _, site := range headerSites.Sites {
+			sites = append(sites, site.URL)
+			removeGouroutine(site.URL)
+		}
+	} else {
+		sites = append(sites, url)
+		removeGouroutine(url)
+	}
+
+	// Removing entries
+	lines := strings.Split(string(content), "\n")
+	var newLines []string
+	for _, line := range lines {
+		if !all && strings.Contains(line, "# Added by selfcontrol") {
+			newLines = append(newLines, line)
+			continue
+		}
+		if !strings.Contains(line, "# Added by selfcontrol") {
+			shouldKeep := true
+			for i := 0; i < len(sites); {
+				site := sites[i]
+				if strings.Contains(line, site) {
+					// Remove the site from the sites array
+					sites = append(sites[:i], sites[i+1:]...) // Remove the matched site
+					shouldKeep = false
+					break
+				} else {
+					i++ // Only increment if no removal
+				}
+			}
+			if shouldKeep {
+				newLines = append(newLines, line)
+			}
+		}
+	}
+
+	// Write back to hosts file
+	return os.WriteFile(hostsFile, []byte(strings.Join(newLines, "\n")), 0644)
+}
+
+// Function to remove goroutine for a specific site
+func removeGouroutine(url string) {
+	mu.Lock()
+	wg.Add(1)
+	if cancel, exists := goroutineContexts[url]; exists { //accessing the goroutine map to find the correct cancel() function for the url
+		cancel() // Cancelling the goroutine using the cancel function found in the map
+		delete(goroutineContexts, url)
+		fmt.Printf("\nCancelled goroutine for site: %s\n", url)
+	}
+	wg.Done()
+	mu.Unlock()
+	wg.Wait()
+}
---
+++ constants.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	// Simplified DateTimeLayout
+	DateTimeLayout = "2006-01-02 15:04:05 -0700"
+	hostsFile      = "/etc/hosts"
+)
